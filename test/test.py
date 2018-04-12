037e3a67cf3900a5818aee91f0fc94874b3fab89
Abel <1043477154@qq.com>
2018-04-12 23:00:10 +0800

    math,random

--- test/test.py
+++ test/test.py
@@ -1,5 +1,17 @@
-abc = [1,2,3,4,5];
-print('第一位是{d[0]}第二位为{d[2]}'.format(d=abc))
-print('第一位是{b}第二位为{a}'.format(a="哈哈", b="呵呵"))
-hex(1000)
-print(pow(3,3))
+# _*_ coding:utf-8 _*_
+import math
+import random
+a = 2.3
+print(math.ceil(a))
+print(math.floor(a))
+print(math.sqrt(9))
+print(math.log(100, 10))
+print(random.random())
+print(random.choice([0, 1, 2, 3, 4, 5]))
+print(random.uniform(1, 5))
+print(random.randint(1, 5))
+print(random.randrange(1, 5))
+
+b = "i am Abel"
+print(b[2])
+
